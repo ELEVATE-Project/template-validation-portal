2f719b223ee0e166b55b974f9582652ba462d3f2
hanamant-pacewisdom <hanamant.k@pacewisdom.com>
2022-09-22 09:33:42 +0530

    Issue #template:changes:template-selection view

--- src/app/modules/template/template-selection/template-selection.component.ts
+++ src/app/modules/template/template-selection/template-selection.component.ts
@@ -10,6 +10,7 @@ export class TemplateSelectionComponent implements OnInit {
 
   selectedFile: any;
   fileInput: any;
+  fileName = ''
 
   public downloadTemplates = [
     { id: 0, name: "Select Project Template" },
@@ -40,5 +41,20 @@ export class TemplateSelectionComponent implements OnInit {
   fileUpload(fileInput: HTMLInputElement) {
     fileInput.click()
   }
+
+  getFileDetails (event:any) {
+    for (var i = 0; i < event.target.files.length; i++) { 
+      this.fileName = event.target.files[i].name;
+      // var type = event.target.files[i].type;
+      // var size = event.target.files[i].size;
+      // var modifiedDate = event.target.files[i].lastModifiedDate;
+      
+      // console.log ('Name: ' + name + "\n" + 
+      //   'Type: ' + type + "\n" +
+      //   'Last-Modified-Date: ' + modifiedDate + "\n" +
+      //   'Size: ' + Math.round(size / 1024) + " KB");
+    }
+  }
+  
 
 }
