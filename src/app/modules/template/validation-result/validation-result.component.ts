19447b4d83276a138c7f6296073657aa3b638e82
GSuma21 <21gsuma@gmail.com>
2022-10-27 10:43:16 -0700

    issue #fix validation_result and select_template

--- src/app/modules/template/validation-result/validation-result.component.ts
+++ src/app/modules/template/validation-result/validation-result.component.ts
@@ -16,12 +16,18 @@ export class ValidationResultComponent implements OnInit {
  
   @ViewChild(MatPaginator, { static: true })
   paginator!: MatPaginator;
-
+  highlight: boolean = false;
   data: MatTableDataSource<any> | undefined;
   columnNames: any;
   result:any;
+  row:any;
+  length:any;
+  sheetarr: any;
+  wsname: any;
+  wbfile: any;
+  a:any;
   fileName: string = 'SheetJS.xlsx';
-
+  // selection: any[] = [3];
   public downloadResources = [
     { id: 0, name: 'Select Project Resource' },
     { id: 1, name: 'Select Observation Resource' },
@@ -29,7 +35,7 @@ export class ValidationResultComponent implements OnInit {
   ];
   constructor() {}
 
-  ngOnInit(): void {}
+  
 
   /**
    * Set the paginator after the view init since this component will
@@ -45,20 +51,43 @@ export class ValidationResultComponent implements OnInit {
     if (target.files.length !== 1) throw new Error('Cannot use multiple files');
     const reader: FileReader = new FileReader();
     this.readFile(target, reader).subscribe((output) => {
-      this.result = output;
-      this.columnNames = Object.keys(this.result[0]);
-      this.data = new MatTableDataSource(this.result);
-      this.data.paginator = this.paginator;
+    //   this.result = output;
+    //   this.columnNames = Object.keys(this.result[0]);
+    //   this.data = new MatTableDataSource(this.result);
+    //  this.length = this.data.data.length;
+    //   this.data.paginator = this.paginator;
+     
+    //    ;
+     
     });
   }
+  ngOnInit(): void {}
 
+
+  isSelected( column:any, ele:any) {
+    
+    // let index = column * this.length + row ;
+    //  return this.selection.indexOf(index) >= 0;
+ 
+   
+   if( ele==this.a && column == 'Name'){
+    return true;
+   }else{
+    return false;
+   }
+
+  }
+
+  
   readFile(target: DataTransfer, reader: FileReader): Observable<string> {
     const sub = new Subject<string>();
 
     reader.onload = (e: any) => {
       const bstr: string = e.target.result;
       const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
-
+      this.wbfile = wb;
+      this.sheetarr = wb.SheetNames;
+      this.wbfile = wb;
       /* grab first sheet */
       const wsname: string = wb.SheetNames[0];
       const ws: XLSX.WorkSheet = wb.Sheets[wsname];
@@ -67,19 +96,34 @@ export class ValidationResultComponent implements OnInit {
       const data: any = XLSX.utils.sheet_to_json(ws);
 
       sub.next(data);
+     
       sub.complete();
+     
     };
 
     reader.readAsBinaryString(target.files[0]);
     return sub.asObservable();
   }
 
+  onClickSheetName(s: any){
+    const wsname: string = s;
+    const ws: XLSX.WorkSheet = this.wbfile.Sheets[wsname];
+    const data: any = XLSX.utils.sheet_to_json(ws);
+    this.columnNames = Object.keys(data[0]);
+    this.data = new MatTableDataSource(data);
+    this.data.paginator = this.paginator;
+    if(s=="Sheet1")
+    this.a = this.data.data[2].Name
+  }
+
   export(): void {
-    const onlyNameAndSymbolArr: Partial<any>[] = this.result;
-    TableUtil.exportArrayToExcel(onlyNameAndSymbolArr, this.fileName);
+    // const onlyNameAndSymbolArr: Partial<any>[] = this.wbfile;
+    // TableUtil.exportArrayToExcel(onlyNameAndSymbolArr, this.fileName);
+
+    XLSX.writeFile(this.wbfile, `$file.xlsx`);
   }
  
-
+  
   
 }
 
